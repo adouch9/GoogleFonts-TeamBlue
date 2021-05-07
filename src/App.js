bf398af0c52716cf4c4721925122cff5f5cc8ca8
Imo999 <rcajina.pro@gmail.com>
2021-05-07 02:42:58 +0200

    Update : Add routing

--- src/App.js
+++ src/App.js
@@ -1,42 +1,18 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.css';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import Text from './components/selection/Text';
-import FontSize from './components/selection/FontSize';
-import ContextProvider from './context/Context';
-import SelectFont from './components/selection/SelectFont';
-import { useState } from 'react';
-import Content from './components/Content';
+import React from 'react'
+import { Switch, Route, Redirect } from 'react-router'
+import Home from './pages/Home'
+
+
+// Création de la route /Home => www.xxx/home.com
+// Posibilité de rajouter des extensions de route / pages
 
 function App() {
-  const [content, setContent] = useState('date');
 
-  const handleSelectChange = (e) => {
-    // console.log(e.target.value);
-    setContent(e.target.value);
-  };
   return (
-    <>
-      <Header />
-      <ContextProvider>
-        <main className='container min-vh-100'>
-          <div className='row my-5'>
-            <div className='col-lg-3'>
-              <div style={{ position: 'sticky', top: '0px' }}>
-                <SelectFont handleSelectChange={handleSelectChange} />
-                <Text />
-                <FontSize />
-              </div>
-            </div>
-            <div className='col-lg-9'>
-              <Content content={content} />
-            </div>
-          </div>
-        </main>
-      </ContextProvider>
-      <Footer />
-    </>
+    <Switch>
+      <Route exact path="/Home" component={Home} />
+      <Redirect to="/Home" />
+    </Switch>
   );
 }
 
